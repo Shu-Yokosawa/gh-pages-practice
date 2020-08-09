bc238e7ca10da0d9a8bddf7411a4008c014f715e
Shu-Yokosawa <yokosawa_saba@icloud.com>
2020-08-09 15:44:29 +0900

    commit test

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+  <router-link to="/">Go to page1</router-link>
+  <router-link to="/page2">Go to page2</router-link>
+    <img src="./assets/curry.jpg">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
